58a77703369b0dc59033a300b986019b91c51830
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-03-02 14:49:03 -0300

    routing and lazy loading

--- learning/src/app/app-routing.module.ts
+++ learning/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'usuario', loadChildren: () => import('./components/usuario/usuario.module').then(m => m.UsuarioModule)},
+  {path: '**', redirectTo: '/', pathMatch: 'full'}
+  //Si la ruta especificada no coincide con ninguna se redirecciona al index
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
