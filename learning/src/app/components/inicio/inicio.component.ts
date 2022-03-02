80ab1a442e2c38d15af966d34ef2209a3a26f749
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-03-02 12:25:01 -0300

    form validation

--- learning/src/app/components/inicio/inicio.component.ts
+++ learning/src/app/components/inicio/inicio.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  error = false;
+  pin = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ingresar(){
+    //Validar que el pin no esté vacio
+    if(this.pin == ''){
+      this.error = true;
+      
+      setTimeout(() => {
+        this.error = false;
+      }, 3000);
+    }
   }
 
 }
