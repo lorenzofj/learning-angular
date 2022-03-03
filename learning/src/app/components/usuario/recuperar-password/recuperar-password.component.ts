ffea521165ef20f65384466f138f3cd9d9c45385
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-03-03 12:01:29 -0300

    recover password and validations

--- learning/src/app/components/usuario/recuperar-password/recuperar-password.component.ts
+++ learning/src/app/components/usuario/recuperar-password/recuperar-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-password',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperar-password.component.css']
 })
 export class RecuperarPasswordComponent implements OnInit {
+  recuperarForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.recuperarForm = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  recuperarPassword(){
+    
+  }
 }
--- learning/src/app/components/usuario/usuario-routing.module.ts
+++ learning/src/app/components/usuario/usuario-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'recuperarPassword', component: RecuperarPasswordComponent}
 ];
 
 @NgModule({
