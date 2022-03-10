7bb222056df88c7347b4f83868b0e2ad8fbeb2b2
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-03-10 12:05:07 -0300

    navbar and logout

--- learning/src/app/components/dashboard/navbar/navbar.component.ts
+++ learning/src/app/components/dashboard/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logOut(){
+    this.afAuth.signOut();
+    this.router.navigate(['/']);
   }
 
 }
