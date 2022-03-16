d0ed0a4e5937c74ef4d4afaf6c5808fb18e17f60
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-03-16 09:33:57 -0300

    user logged in verification and unsubscribe

--- learning/src/app/components/dashboard/list-cuestionarios/list-cuestionarios.component.ts
+++ learning/src/app/components/dashboard/list-cuestionarios/list-cuestionarios.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-cuestionarios',
   templateUrl: './list-cuestionarios.component.html',
   styleUrls: ['./list-cuestionarios.component.css']
 })
-export class ListCuestionariosComponent implements OnInit {
+export class ListCuestionariosComponent implements OnInit, OnDestroy {
+  suscriptionUser: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.suscriptionUser = this.afAuth.user.subscribe(user => {
+      if(user && user.emailVerified){
+        
+      }
+      else{
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.suscriptionUser.unsubscribe();
   }
 
 }
