be62f45ac3397227cb2a82c54e34f21e26fa822d
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-04-07 11:48:28 -0300

    contador-inicial component

--- learning/src/app/components/jugar/jugar-routing.module.ts
+++ learning/src/app/components/jugar/jugar-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContadorInicialComponent } from './contador-inicial/contador-inicial.component';
 import { IngresarNombreComponent } from './ingresar-nombre/ingresar-nombre.component';
+import { RealizarQuizzComponent } from './realizar-quizz/realizar-quizz.component';
 
 const routes: Routes = [
   {path: '', component: IngresarNombreComponent},
-  {path: 'iniciarContador', component: ContadorInicialComponent}
+  {path: 'iniciarContador', component: ContadorInicialComponent},
+  {path: 'realizarQuizz', component: RealizarQuizzComponent}
 ];
 
 @NgModule({
