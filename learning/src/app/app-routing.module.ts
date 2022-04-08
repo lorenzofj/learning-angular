c03f7cf2a8c62cf5599f43d49fc6da596784410f
lorenzofj <76181814+lorenzofj@users.noreply.github.com>
2022-04-08 14:58:01 -0300

    guards

--- learning/src/app/app-routing.module.ts
+++ learning/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'usuario', loadChildren: () => import('./components/usuario/usuario.module').then(m => m.UsuarioModule)},
-  {path: 'dashboard', component: DashboardComponent, loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule)},
+  {path: 'dashboard', component: DashboardComponent, loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard]},
   {path: '**', redirectTo: '/', pathMatch: 'full'},
     //Si la ruta especificada no coincide con ninguna se redirecciona al index
   {path: 'jugar', loadChildren: () => import('./components/jugar/jugar.module').then(m => m.JugarModule)}
